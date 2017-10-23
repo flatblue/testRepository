42870e7985d5be2a9f73834544b68ee61fed37c0
DESKTOP-DINDDB3\sleep <pleasepurple@gmail.com>
2017-10-23 22:45:59 +0900

    test

--- Main.java
+++ Main.java
@@ -3,8 +3,10 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Here your code !
+        // HEllo Got
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
+
+	// you get my power
     }
 }
