532619f343c51381eafa675dcf5f910ef84fa6a3
flatblue <33028703+flatblue@users.noreply.github.com>
2017-10-23 22:18:51 +0900

    Test 1
    test1

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Here your code !
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+    }
+}
